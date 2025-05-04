bc411d90a4488ff3e4d7f6b637192bf444f9d8c9
pradikafajar <110707425+pradikafajar@users.noreply.github.com>
2025-05-04 23:31:13 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Tambahkan ini kalau pakai gambar/image dari Next
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
+
